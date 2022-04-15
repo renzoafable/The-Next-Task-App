ddaf616372e4ade5f057166e4149031739bfea81
renzoafable <afablelorenzmatthew@gmail.com>
2022-04-16 00:18:00 +0800

    Add register page

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,19 +3,28 @@ import Container from 'react-bootstrap/Container';
 
 import Header from 'src/components/Header';
 import TaskInput from 'src/components/TaskInput';
+import { useAppState } from 'src/context/AppContext';
 
 type AppContainerProps = {
   children: React.ReactNode;
 };
 
 export default function AppContainer({ children }: AppContainerProps) {
+  const { user } = useAppState();
+
+  const authenticatedContent = user ? (
+    <>
+      <Header />
+      <TaskInput />
+    </>
+  ) : null;
+
   return (
     <Container
       fluid="md"
-      className="shadow-lg h-75 w-50 rounded-2 p-4 overflow-auto"
+      className="shadow-lg h-75 w-50 rounded-2 p-4 overflow-auto position-relative"
     >
-      <Header />
-      <TaskInput />
+      {authenticatedContent}
       {children}
     </Container>
   );
