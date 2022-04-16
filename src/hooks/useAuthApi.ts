a7b69fe024ee0c660668a674b88a39434085a9ae
renzoafable <afablelorenzmatthew@gmail.com>
2022-04-16 22:03:19 +0800

    Simplify formatting of auth hooks by placing useCallback at the return object

--- src/hooks/useAuthApi.ts
+++ src/hooks/useAuthApi.ts
@@ -10,28 +10,25 @@ export function useRegisterUser() {
   const [data, setData] = useState<AuthResponse | null>(null);
   const [error, setError] = useState<unknown>(null);
 
-  const execute = useCallback(
-    async (userPayload: AuthUserPayload): Promise<void> => {
-      try {
-        setIsLoading(true);
+  const execute = async (userPayload: AuthUserPayload): Promise<void> => {
+    try {
+      setIsLoading(true);
 
-        const response = await axios.post<AuthResponse>(
-          '/user/register',
-          userPayload
-        );
+      const response = await axios.post<AuthResponse>(
+        '/user/register',
+        userPayload
+      );
 
-        setData(response.data);
-        setUser(response.data.user);
-        localStorage?.setItem('todoAuthToken', response.data.token);
-        setIsLoading(false);
-      } catch (err: unknown) {
-        setError(err);
-      }
-    },
-    []
-  );
+      setData(response.data);
+      setUser(response.data.user);
+      localStorage?.setItem('todoAuthToken', response.data.token);
+      setIsLoading(false);
+    } catch (err: unknown) {
+      setError(err);
+    }
+  };
 
-  return { execute, data, isLoading, error };
+  return { data, error, execute: useCallback(execute, []), isLoading };
 }
 
 export function useLogin() {
@@ -40,26 +37,23 @@ export function useLogin() {
   const [data, setData] = useState<AuthResponse | null>(null);
   const [error, setError] = useState<unknown>(null);
 
-  const execute = useCallback(
-    async (loginPayload: AuthLoginPayload): Promise<void> => {
-      try {
-        setIsLoading(true);
+  const execute = async (loginPayload: AuthLoginPayload): Promise<void> => {
+    try {
+      setIsLoading(true);
 
-        const response = await axios.post<AuthResponse>(
-          '/user/login',
-          loginPayload
-        );
+      const response = await axios.post<AuthResponse>(
+        '/user/login',
+        loginPayload
+      );
 
-        setData(response.data);
-        setUser(response.data.user);
-        localStorage?.setItem('todoAuthToken', response.data.token);
-        setIsLoading(false);
-      } catch (err: unknown) {
-        setError(err);
-      }
-    },
-    []
-  );
+      setData(response.data);
+      setUser(response.data.user);
+      localStorage?.setItem('todoAuthToken', response.data.token);
+      setIsLoading(false);
+    } catch (err: unknown) {
+      setError(err);
+    }
+  };
 
-  return { data, error, execute, isLoading };
+  return { data, error, execute: useCallback(execute, []), isLoading };
 }
