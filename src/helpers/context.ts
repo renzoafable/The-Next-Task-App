f5ec9722c615a4780fc0b73abffa878b1f88087c
renzoafable <afablelorenzmatthew@gmail.com>
2022-04-16 20:55:22 +0800

    Add comments to the context helper file

--- src/helpers/context.ts
+++ src/helpers/context.ts
@@ -1,5 +1,11 @@
 import { createContext, useContext } from 'react';
 
+/**
+ * A utility function that creates a context without Typescript shouting that we haven't provided
+ * a default context value.
+ *
+ * See https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/context for details.
+ */
 export default function createCtx<A>() {
   const ctx = createContext<A | undefined>(undefined);
   function useCtx() {
